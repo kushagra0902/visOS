0d2c7eef4257255b6664d6188d4d0182b0c8cd96
Priyanshu <Priyanshukhedekar99@gmail.com>
2026-03-18 17:34:45 +0530

    added io folder

---
+++ src/services/io/io_device_simulator.cpp
@@ -0,0 +1,8 @@
+#include "services/io/io_device_simulator.hpp"
+
+namespace visos {
+
+// I/O completion is currently modeled via BlockedQueue wakeup ticks.
+// This file exists as a placeholder for future device simulation.
+
+} // namespace visos
---
+++ src/services/io/io_device_simulator.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace visos {
+
+// Placeholder for future I/O device simulation.
+// Currently, I/O completion is modeled through the BlockedQueue's wakeup_tick mechanism.
+class IoDeviceSimulator {
+public:
+    IoDeviceSimulator() = default;
+};
+
+} // namespace visos
